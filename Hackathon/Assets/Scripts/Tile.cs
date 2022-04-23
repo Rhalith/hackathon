f25a3f0c7f7ecd6f85c8dd195129691cc87b1461
Reha Buğrahan Özel <82326121+Hydofbl@users.noreply.github.com>
2022-04-23 14:55:54 +0300

    Tile modules are created.

--- Hackathon/Assets/Scripts/Tile.cs
+++ Hackathon/Assets/Scripts/Tile.cs
@@ -1,15 +1,24 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class Tile : MonoBehaviour
 {
     public PlayerController playerController;
+    public GameManager gameManager;
 
-    public int tileX;
-    public int tileY;
+    public bool isClickable = true;
+    public bool isOneTimeUse = false;
+
+    public float timeCost;
+
+    public float stormFactor = 1f;
+
+    private int tileX;
+    private int tileY;
 
     private void Awake()
     {
@@ -21,7 +30,16 @@ public class Tile : MonoBehaviour
     {
         int maxMoveDistance = playerController.maxMoveDistance;
         
-        if(Mathf.Abs(playerController.posX-tileX) <= maxMoveDistance && Mathf.Abs(playerController.posY-tileY) <= maxMoveDistance)
+        // time coroutine
+        gameManager.rescueTime -= timeCost * stormFactor;
+        print(gameManager.rescueTime);
+        
+        if(isClickable && (Mathf.Abs(playerController.posX-tileX) <= maxMoveDistance && Mathf.Abs(playerController.posY-tileY) <= maxMoveDistance))
             StartCoroutine(playerController.MovePlayerTo(tileX, tileY));
+    }
+
+    private void OnMouseOver()
+    {
+     //   print("isClickable: " + isClickable);
     }
 }
